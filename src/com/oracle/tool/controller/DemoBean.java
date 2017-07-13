7db3e983424a0e210969a4fea6bc254c3b94839b
raparash <raju.parashar@oracle.com>
2017-07-13 23:09:19 +0530

    Baseline

---
+++ src/com/oracle/tool/controller/DemoBean.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.oracle.tool.controller;
+
+import com.oracle.web.fmwk.JsonBean;
+
+/**
+ * @author raparash
+ *
+ */
+public class DemoBean implements JsonBean {
+
+	private String firstName;
+	private String lastName;
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+}
